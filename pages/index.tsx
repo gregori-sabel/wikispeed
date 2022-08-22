dbca641551c552f0c213f41503e32ce444a4a5f2
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2022-08-22 19:52:19 -0300

    Mudando links

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,10 @@ export default function Home() {
 
   // ao clicar num link, chama a nova pagina da wiki
   function handleClickedLink(event, link: string) {
-    event.preventDefault()
-    console.log(link)
-
-    fetch(`https://en.wikipedia.org/api/rest_v1/page/html/${link}`)
+    const htmlPage = link.replace(
+      'http://en.wikipedia.org/wiki/', 
+      'https://en.wikipedia.org/api/rest_v1/page/html/')
+    fetch(htmlPage)
       .then(res => res.text())
       .then(text => setPotato(text))
   }
@@ -20,44 +20,43 @@ export default function Home() {
       .then(text => setPotato(text))
 
     // tentativa falha de alterar as tags
-    const aTags = document.getElementsByTagName('a');
-    const aTagsList = Array.from(aTags);
-    aTagsList.map(tag => {
-      tag.onclick = () => handleClickedLink(event, tag.href)
-      // tag.href = '#'
-    })
-      
-    // outra tentativa falha de alterar as tags
-    // const ases = document.getElementsByTagName('a');
-    // var arr = Array.from(ases);
-    // arr.map(ar => {
-    //   ar.href = '#'
-    //   ar.onclick = () => {
-    //     console.log('clicou fei')
-    //   }
-    //   console.log(ar.href)
+    // const aTags = document.getElementsByTagName('a');
+    // const aTagsList = Array.from(aTags);
+    // aTagsList.map(tag => {
+    //   tag.onclick = () => handleClickedLink(event, tag.href)
+    //   // tag.href = '#'
     // })
   },[])
 
+  useEffect(() => {
+    const ases = document.getElementsByTagName('a');
+    var arr = Array.from(ases);
+    arr.map(ar => {
+      ar.onclick = (event) => {
+        handleClickedLink(event, ar.href)
+        return false // retorna false para não abrir o link
+      }
+    })
+  }, [ potato ])
 
   return (
     <>
       {/* tags para tentar alterar  */}
-      <h1>Helo uma ova!</h1>
+      {/* <h1>Helo uma ova!</h1>
       <a href="/potato">coisas loucas</a>
       <br />
       <a href="/batata">coisas loucas</a>
       <br />
-      <a href="/polvora">coisas loucas</a>
+      <a href="/polvora">coisas loucas</a> */}
 
       {/* A pagina da wiki, comentada pra teste*/}
-      {/* { potato && (
+      { potato && (
         <>
           <div 
             dangerouslySetInnerHTML={{__html: potato}}
           />
         </>
-      )} */}
+      )}
     </>
   )
 }
