fe5c376ceb44a2ab1e80c3e69ebd792600f524b5
Grégori Sabel <seriosabel57@gmail.com>
2022-08-24 15:56:22 -0300

    theme

--- src/components/WikiPage/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.div`
-  width: 1000px;
-`
-
-export const Title = styled.h1`
-
-`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
+import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
-import Home from "./Game/index";
 
 export default function Base() {
-  return <Home/>
+  return (
+    <Flex
+      w='100%'
+      height='100%'
+      justify='center'
+      align='center'
+      backgroundColor='brand.500'
+    >
+      <h1>oie, indx aqui</h1>
+    </Flex>
+  )
 }
