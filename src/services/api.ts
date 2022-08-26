c736c9cd74455ef01d92806bbe2424c4482a954b
Grégori Sabel <seriosabel57@gmail.com>
2022-08-26 17:48:40 -0300

    no cors try: 4

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create ({
-  headers: { 
-    "Access-Control-Allow-Origin": "*"
-  }
+  // headers: {"Access-Control-Allow-Origin": "*"}
 })
 
+api.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export { api }
